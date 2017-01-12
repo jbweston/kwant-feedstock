c4732dfaba826c950bdeaf8edf01a7e1246dab0c
Bas Nijholt <basnijholt@gmail.com>
2017-01-12 10:33:14 +0100

    Add Python 3.6  (#11)
    * MNT: Re-rendered with conda-smithy 1.7.0 and add Python 3.6 for Linux
    
    * MNT: Re-rendered with conda-smithy 1.7.0

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -49,7 +49,7 @@ source run_conda_forge_build_setup
 yum install -y devtoolset-2-gcc-gfortran
 
 
-# Embarking on 4 case(s).
+# Embarking on 6 case(s).
     set -x
     export CONDA_NPY=110
     export CONDA_PY=34
@@ -74,6 +74,20 @@ yum install -y devtoolset-2-gcc-gfortran
     set -x
     export CONDA_NPY=111
     export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=110
+    export CONDA_PY=36
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=111
+    export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
