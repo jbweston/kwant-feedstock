65ff8484aaea3ad1f1b1dc33b1a502053b9809ff
Joseph Weston <joseph.weston08@gmail.com>
2017-05-29 18:36:04 +0200

    fix build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
 dir %PREFIX%\Library\lib
-%PYTHON% -c "with open(r'%RECIPE_DIR%\build_windows.conf', 'r+') as f: c = f.read(); f.seek(0); f.write(c.replace('PREFIX', '%PREFIX%'))"
+%PYTHON% -c "with open(r'%RECIPE_DIR%\build_windows.conf', 'r+') as f: c = f.read(); f.seek(0); f.write(c.replace('PREFIX', r'%PREFIX%'))"
 
 %PYTHON% setup.py install --configfile=%RECIPE_DIR%\build_windows.conf --single-version-externally-managed --record=record.txt
