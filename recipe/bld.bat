612b2c01461f4a48377a4b9fcdc4fe2d0b4a3795
Joseph Weston <joseph.weston08@gmail.com>
2017-05-29 17:56:40 +0200

    use python as sed

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
-powershell -Command "get-content %RECIPE_DIR%\build_windows.conf | %{$_ -replace 'PREFIX','%PREFIX%'}"
+%PYTHON% -c "with open(r'%RECIPE_DIR%\build_windows.conf', 'r+') as f: c = f.read(); f.seek(0); f.write(c.replace('PREFIX', '%PREFIX%'))"
 
 %PYTHON% setup.py install --configfile=%RECIPE_DIR%\build_windows.conf --single-version-externally-managed --record=record.txt
