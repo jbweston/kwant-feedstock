a36d7ce9c5afe7932dd75fca69022c24f5518ce6
Joseph Weston <joseph.weston08@gmail.com>
2017-05-29 18:30:26 +0200

    print contents of library dir

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
+dir %PREFIX%\Library\lib
 %PYTHON% -c "with open(r'%RECIPE_DIR%\build_windows.conf', 'r+') as f: c = f.read(); f.seek(0); f.write(c.replace('PREFIX', '%PREFIX%'))"
 
 %PYTHON% setup.py install --configfile=%RECIPE_DIR%\build_windows.conf --single-version-externally-managed --record=record.txt
