64518c3f27a1e34d5ce1b4147627022dd7a29fc2
Filipe Fernandes <ocefpaf@gmail.com>
2017-07-20 14:42:00 -0300

    rerender with auto

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -65,30 +65,14 @@ source run_conda_forge_build_setup
 /usr/bin/sudo -n yum install -y devtoolset-2-gcc-gfortran
 
 
-# Embarking on 4 case(s).
+# Embarking on 2 case(s).
     set -x
-    export CONDA_NPY=111
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
-    export CONDA_NPY=112
-    export CONDA_PY=35
-    set +x
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
-    set -x
-    export CONDA_NPY=111
-    export CONDA_PY=36
-    set +x
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
-    set -x
-    export CONDA_NPY=112
     export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
