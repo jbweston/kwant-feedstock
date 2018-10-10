27ea534f327ec90bc55894802f8e2fee7ec76ad0
Joseph Weston <joseph@weston.cloud>
2018-10-10 17:19:47 +0200

    add windows-specific build config file

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,6 @@
+:: replace PREFIX with the contents of the env var %PREFIX%
+set build_template="%RECIPE_DIR%\build_windows.conf"
+set build_target="build.conf"
+powershell -Command "(gc  %build_template%) -replace PREFIX, %PREFIX% | Out-File %build_target% -encoding utf8"
+
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
