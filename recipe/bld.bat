71637b11b2703d7cbeceeed73cc535d62670fff2
Joseph Weston <joseph@weston.cloud>
2019-02-17 14:30:08 +0100

    Try different powershell command to create build file
    This should produce a sane error message if the file does not exist
    for some reason

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,8 @@
 :: replace PREFIX with the contents of the env var %PREFIX%
 set build_template="%RECIPE_DIR%\build_windows.conf"
 set build_target="build.conf"
-powershell -Command "(gc  %build_template%) -replace PREFIX, %PREFIX% | Out-File %build_target% -encoding utf8"
+powershell -Command "(Get-Content -path %build_template% -Raw) -replace 'PREFIX','%PREFIX%' | Set-Content -Path %build_target%"
+:: debug
+powershell -Command "Get-Content %build_target%"
 
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
